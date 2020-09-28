d627f2f2d3d0fd1a6f5b293b1e5daadcd50f18bf
bb515 <bb515@cam.ac.uk>
2020-09-28 08:33:44 +0100

    Changed sys path for docs build to see if that helps.

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+# sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('.peripy'))
 
 # Add type of source files
 source_suffix = ['.rst', '.md']
