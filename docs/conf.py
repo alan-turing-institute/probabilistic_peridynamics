2756d616eb0625561ec5bd5f059fa4a9b6b022e8
bb515 <bb515@cam.ac.uk>
2020-09-28 08:38:51 +0100

    changed sys path again

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('.peripy'))
+sys.path.insert(0, os.path.abspath('../peripy'))
 
 # Add type of source files
 source_suffix = ['.rst', '.md']
